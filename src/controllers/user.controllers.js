87c6c71c640360c217b553c4c989268ae24bb18f
Raj Soren <7rajkumarsoren@gmail.com>
2025-02-14 21:11:29 +0530

    feat: add user password change functionality and create models for comments, likes, playlists, subscriptions, and tweets

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -233,4 +233,23 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
     }
 })
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken}
+const changeCurrentPassword = asyncHandler(async (req,res) => {
+    const {oldPassword, newPassword} = req.body;
+
+    // find the user from the database
+    const user = await User.findById(req.user?._id);
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if(!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password");
+    }
+
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false});
+    // validateBeforeSave: false ensures that only password changes are saved, skipping other model validations
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{}, "Password changed successfully"))
+})
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword}
